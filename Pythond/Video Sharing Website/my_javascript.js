590ccca736251e1316b573c872761203b533a106
FahimFaisalRahman <faisalfahim003@gmail.com>
2025-12-17 19:27:02 +0600

    Add files via upload

---
+++ Pythond/Video Sharing Website/my_javascript.js
@@ -0,0 +1,8 @@
+
+// saved_javascript.js
+var menuIcon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+
+menuIcon.click = function(){
+    sidebar.class.toggle("small-sidebar");
+    }
